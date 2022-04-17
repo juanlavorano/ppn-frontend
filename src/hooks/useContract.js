633bab97a060a4c6616fd42369322386d5ce8478
juanlavorano <juanmlavorano@gmail.com>
2022-04-17 18:51:41 +0200

    added hook for interacting with contract

--- src/hooks/useContract.js
+++ src/hooks/useContract.js
@@ -1,10 +1,23 @@
+import { Contract } from "ethers";
 import { PPN_ABI } from "constants/abi";
 import { PPN_ADDRESS } from "constants/address";
-import useReefNetwork from "@hooks/useReefNetwork";
-import { Signer } from "@reef-defi/evm-provider";
+import useAccounts from "@hooks/useAccounts";
+import { useCallback, useEffect, useState } from "react";
 
 export default function useContract() {
-  const { provider } = useReefNetwork();
+  const { signer } = useAccounts();
+  const [contract, setContract] = useState(null);
 
-  return "hola";
+  const instantiateContract = useCallback(() => {
+    let contract;
+    if (signer) contract = new Contract(PPN_ADDRESS, PPN_ABI, signer);
+
+    return contract;
+  }, [signer]);
+
+  useEffect(() => {
+    setContract(instantiateContract());
+  }, [instantiateContract]);
+
+  return contract;
 }
