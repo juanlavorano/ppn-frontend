614eae587656a37b3d4e27028dc7c174bfe31708
juanlavorano <juanmlavorano@gmail.com>
2022-05-14 19:56:45 +0200

    fixed message

--- src/constants/notifications.js
+++ src/constants/notifications.js
@@ -1,4 +1,4 @@
 export const mint = {
   success: "Successfully minted! Checkout the Collection page",
-  error: "OOps! Something went wrong. Try again later",
+  error: "Oops! Something went wrong. Try again later",
 };
